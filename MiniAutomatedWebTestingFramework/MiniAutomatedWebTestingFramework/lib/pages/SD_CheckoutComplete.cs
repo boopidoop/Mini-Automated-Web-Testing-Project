e6fcd0f08c0cf6bba93d459d52110f0ea0cc42ab
JonOfScience <jdcrofts31@gmail.com>
2022-08-15 15:34:01 +0100

    POM for Checkout Complete

---
+++ MiniAutomatedWebTestingFramework/MiniAutomatedWebTestingFramework/lib/pages/SD_CheckoutComplete.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAutomatedWebTestingFramework.lib.pages
+{
+    public class SD_CheckoutComplete
+    {
+        #region Parameters
+        private IWebDriver _seleniumDriver;
+        private string _CheckoutCompletedPageUrl = AppConfigReader.CheckoutComplete;
+
+        private IWebElement _completionText => _seleniumDriver.FindElement(By.ClassName("complete-text"));
+        private IWebElement _completionImage => _seleniumDriver.FindElement(By.ClassName("pony_express"));
+        private IWebElement _backHomeButton => _seleniumDriver.FindElement(By.Id("back-to-products"));
+        #endregion
+
+        #region Methods
+        public string GetCompletionText() => _completionText.Text;
+        public void ClickBackHomeButton() => _backHomeButton.Click();
+        #endregion
+    }
+}
