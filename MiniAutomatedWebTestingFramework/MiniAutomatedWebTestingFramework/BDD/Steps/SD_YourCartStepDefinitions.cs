f11228a3e95de9be37d1da1b7a6f46c3597635ca
Mafundii <makshadys1@gmail.com>
2022-08-16 10:33:47 +0100

    Cart_tests_implementation
    Janky tests

--- MiniAutomatedWebTestingFramework/MiniAutomatedWebTestingFramework/BDD/Steps/SD_YourCartStepDefinitions.cs
+++ MiniAutomatedWebTestingFramework/MiniAutomatedWebTestingFramework/BDD/Steps/SD_YourCartStepDefinitions.cs
@@ -17,15 +17,15 @@ namespace MiniAutomatedWebTestingFramework.BDD.Steps
             SD_Website.SD_SignInPage.VisitSignInPage();
             SD_Website.SD_SignInPage.FillUsername("standard_user");
             SD_Website.SD_SignInPage.FillPassword("secret_sauce");
-
-            //Products page
-            SD_Website.SD_ProductsPage.AddItemToCart("add-to-cart-sauce-labs-backpack");
-            SD_Website.SD_ProductsPage.VisitYourCart();
+            SD_Website.SD_SignInPage.ClickLogin();
         }
 
         [Given(@"I have items\(s\) in the cart")]
         public void GivenIHaveItemsSInTheCart()
         {
+            SD_Website.SD_ProductsPage.AddItemToCart("add-to-cart-sauce-labs-backpack");
+            SD_Website.SD_ProductsPage.VisitYourCart();
+
             Assert.That(SD_Website.SD_YourCartPage.GetNumberOfItems(), Is.GreaterThanOrEqualTo(1));
         }
 
@@ -35,10 +35,62 @@ namespace MiniAutomatedWebTestingFramework.BDD.Steps
             SD_Website.SD_YourCartPage.ClickCheckout();
         }
 
+        [Then(@"I should not see any items in list")]
+        public void ThenIShouldNotSeeAnyItemsInList()
+        {
+            Assert.That(SD_Website.SD_YourCartPage.GetNumberOfItems(), Is.EqualTo(0));
+        }
+
+        [Then(@"I should not be able to checkout")]
+        public void ThenIShouldNotBeAbleToCheckout()
+        {
+            SD_Website.SD_YourCartPage.ClickCheckout();
+            Assert.That(SD_Website.SeleniumDriver.Url, Is.EqualTo("https://www.saucedemo.com/cart.html"));
+        }
+
         [Then(@"I should be taken to the Checkout: Your information page")]
         public void ThenIShouldBeTakenToTheCheckoutYourInformationPage()
         {
             Assert.That(SD_Website.SeleniumDriver.Url, Is.EqualTo("https://www.saucedemo.com/checkout-step-one.html"));
         }
+
+        [When(@"I have items\(s\) in the cart")]
+        public void WhenIHaveItemsSInTheCart()
+        {
+            SD_Website.SD_ProductsPage.AddItemToCart("add-to-cart-sauce-labs-backpack");
+            SD_Website.SD_ProductsPage.VisitYourCart();
+
+            Assert.That(SD_Website.SD_YourCartPage.GetNumberOfItems(), Is.GreaterThanOrEqualTo(1));
+        }
+
+
+        [When(@"I click the Continue Shopping button")]
+        public void WhenIClickTheContinueShoppingButton()
+        {
+            SD_Website.SD_ProductsPage.VisitYourCart();
+            SD_Website.SD_YourCartPage.ClickContinueShopping();
+        }
+
+        [Then(@"I should be taken to Products page")]
+        public void ThenIShouldBeTakenToProductsPage()
+        {
+            Assert.That(SD_Website.SeleniumDriver.Url, Is.EqualTo("https://www.saucedemo.com/inventory.html"));
+        }
+
+        [Given(@"I have no items in the cart")]
+        public void GivenIHaveNoItemsInTheCart()
+        {
+            SD_Website.SD_ProductsPage.VisitYourCart();
+            Assert.That(SD_Website.SD_YourCartPage.GetNumberOfItems(), Is.EqualTo(0));
+        }
+
+
+        [AfterScenario]
+        public void DisposeWebDriver()
+        {
+            SD_Website.SeleniumDriver.Dispose();
+            SD_Website.SeleniumDriver.Quit();
+        }
+
     }
 }
